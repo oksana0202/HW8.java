360d5ab1cde981297ea898a237fd1c38d8ca5372
Oksana Ilyakova <93376743+oksana0202@users.noreply.github.com>
2022-01-27 14:22:30 +0300

    Update WeatherModel.java

--- WeatherModel.java
+++ WeatherModel.java
@@ -4,4 +4,6 @@ import java.io.IOException;
 
 public interface WeatherModel {
     String getWeather(String selectedCity, Period period) throws IOException;
+
+    List<Weather> getSavedToDBWeather();
 }
